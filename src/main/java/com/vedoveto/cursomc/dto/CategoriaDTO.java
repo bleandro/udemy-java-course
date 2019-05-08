e025eedadfc59a94dadf43799ef2827270780a75
Bruno Vedoveto Leandro <brunoovedoveto@gmail.com>
2019-05-08 17:39:45 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/vedoveto/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,38 @@
+package com.vedoveto.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.vedoveto.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO()
+	{
+	}
+	
+	public CategoriaDTO(Categoria categoria) {
+		id = categoria.getId();
+		nome = categoria.getNome();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
--- src/main/java/com/vedoveto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vedoveto/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.vedoveto.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vedoveto.cursomc.domain.Categoria;
+import com.vedoveto.cursomc.dto.CategoriaDTO;
 import com.vedoveto.cursomc.services.CategoriaService;
 
 @RestController
@@ -51,5 +54,13 @@ public class CategoriaResource {
 		categoriaService.delete(id);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> listCategoria = categoriaService.findAll();
+		List<CategoriaDTO> listDTO = listCategoria.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); 
+		
+		return ResponseEntity.ok(listDTO);
 	}
 }
