4aad9fc6746fa47817cc9ff01ef2de6e0bc193f6
Bruno Vedoveto Leandro <brunoovedoveto@gmail.com>
2019-06-11 21:50:08 -0300

    Transaction

--- src/main/java/com/vedoveto/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/vedoveto/cursomc/repositories/ProdutoRepository.java
@@ -13,7 +13,7 @@ import com.vedoveto.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-	@Transactional(readOnly=true)
+	@Transactional(readOnly=true) 
 	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
