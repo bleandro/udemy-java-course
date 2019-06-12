d306476973f198e36e2c38099ee03b252b1b8280
Bruno Vedoveto Leandro <brunoovedoveto@gmail.com>
2019-06-11 22:28:37 -0300

    Inserind pedido

---
+++ src/main/java/com/vedoveto/cursomc/services/BoletoService.java
@@ -0,0 +1,17 @@
+package com.vedoveto.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.vedoveto.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(calendar.getTime());
+	}
+}
--- src/main/java/com/vedoveto/cursomc/services/PedidoService.java
+++ src/main/java/com/vedoveto/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.vedoveto.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vedoveto.cursomc.domain.ItemPedido;
+import com.vedoveto.cursomc.domain.PagamentoComBoleto;
 import com.vedoveto.cursomc.domain.Pedido;
+import com.vedoveto.cursomc.domain.Produto;
+import com.vedoveto.cursomc.domain.enums.EstadoPagamento;
+import com.vedoveto.cursomc.repositories.ItemPedidoRepository;
+import com.vedoveto.cursomc.repositories.PagamentoRepository;
 import com.vedoveto.cursomc.repositories.PedidoRepository;
 import com.vedoveto.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,12 +20,48 @@ import com.vedoveto.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 	
 	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido findById(Integer id) {
 		Optional<Pedido> categoria = pedidoRepository.findById(id);
 		return categoria.orElseThrow(
 				() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = pedidoRepository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			Produto produto = produtoService.findById(ip.getProduto().getId());
+			ip.setPreco(produto.getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		
+		return obj;
 	}
 	
 }
